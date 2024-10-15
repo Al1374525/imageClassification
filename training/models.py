e9559dd76f178d185c90d621cc63ee8c2c11fed2
Al1374525 <albertayiteyadjin@gmail.com>
2024-10-15 11:46:15 -0400

    acheived a test accuracy of 0.9197

---
+++ training/models.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn 
+import torchvision
+import torchvision.transforms as transforms
+
+class ImageClassificationModel(nn.Module):
+    def __init__(self):
+        super(ImageClassificationModel, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 8 * 8, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+    
+    def forward(self, x):
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 8 * 8)
+        x = nn.functional.relu(self.fc1(x))
+        x = nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+def initialize_model(device):
+    model = ImageClassificationModel()
+    model.to(device)
+    return model
